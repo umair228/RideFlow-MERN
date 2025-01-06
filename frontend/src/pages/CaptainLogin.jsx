579eca81810dc0976f45684687d12dec9b269d9a
umair <umairkh8251@gmail.com>
2025-01-07 00:27:04 +0500

    build login and signup forms for users/captain

--- frontend/src/pages/CaptainLogin.jsx
+++ frontend/src/pages/CaptainLogin.jsx
@@ -1,8 +1,61 @@
-import React from 'react';
+import React, {useState} from 'react';
+import FormsHeader from "./FormsHeader.jsx";
+import {Link} from "react-router-dom";
 
 const CaptainLogin = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [captainData, setCaptainData] = useState({});
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+
+        setCaptainData({
+            email,
+            password
+        });
+
+        setEmail('');
+        setPassword('');
+
+    }
     return (
-        <div>
+        <div className='p-7 h-screen flex flex-col justify-between'>
+            <div>
+                <FormsHeader/>
+                <form onSubmit={submitHandler}>
+                    <h3 className='text-lg font-medium mb-2'>What's your email</h3>
+                    <input
+                        className='bg-[#eeeeee] rounded px-4 py-2 border w-full text-lg placeholder:text-base'
+                        required
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        type="email"
+                        placeholder="email@example.com"
+                    />
+                    <h3 className='text-lg font-medium'>Enter your password</h3>
+
+                    <input
+                        className='bg-[#eeeeee] rounded px-4 py-2 border w-full text-lg placeholder:text-base'
+                        required
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        type="password"
+                        placeholder="password"
+                    />
+                    <button className='bg-[#111] text-white font-semibold mt-7 mb-7 rounded px-4 py-2 w-full text-lg'>
+                        Login
+                    </button>
+                </form>
+                <div>
+                    <p className='text-center'>Register with us? <Link to='/captain-signup' className='text-blue-500'> Become a captain!</Link></p>
+                </div>
+            </div>
+            <div>
+                <Link to='/login' className='inline-flex justify-center border-4 bg-[orange] text-white font-semibold mt-7 mb-7 rounded px-4 py-2 w-full text-lg'>
+                    Login as User
+                </Link>
+            </div>
 
         </div>
     );
---
+++ frontend/src/pages/FormsHeader.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FormsHeader = () => {
+    return (
+        <div>
+            <img className='w-16 mb-4' src='https://static.thenounproject.com/png/3381642-200.png' alt='rideflow logo'/>
+        </div>
+    );
+};
+
+export default FormsHeader;
