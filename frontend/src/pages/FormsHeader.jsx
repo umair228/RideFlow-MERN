dc753e6a11b91fb1968984f851c08068cba458a6
umair <umairkh8251@gmail.com>
2025-01-14 00:53:04 +0500

    Riding page UI for users

--- frontend/src/pages/FormsHeader.jsx
+++ frontend/src/pages/FormsHeader.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const FormsHeader = () => {
     return (
         <div>
-            <img className='w-16 absolute left-5 top-5' src='https://static.thenounproject.com/png/3381642-200.png' alt='rideflow logo'/>
+            <img className='w-16 mb-4' src='https://static.thenounproject.com/png/3381642-200.png' alt='rideflow logo'/>
         </div>
     );
 };
