6231a32e725f52d2ea56c058c91799fc842ce639
umair <umairkh8251@gmail.com>
2025-01-13 21:26:09 +0500

    Creating user home screen UI

---
+++ frontend/src/components/LocationSearchPanel.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LocationSearchPanel = () => {
+    return (
+        <div>
+        LocationSearchPanel
+        </div>
+    );
+};
+
+export default LocationSearchPanel;
